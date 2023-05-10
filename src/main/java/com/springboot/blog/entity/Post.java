ec9a2afd4f130a3683c2165c57629179a7be8410
anmol963 <anmolsingh0098765@gmail.com>
2023-05-10 23:21:11 +0530

    created POST jpa entity

---
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -0,0 +1,32 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true, nullable = false)
+	private String title;
+	
+	@Column(nullable = false)
+	private String description;
+	
+	@Column(nullable = false)
+	private String content;
+}
